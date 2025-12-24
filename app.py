d5b01502fff88804f4104117135013ca7d1b0caa
phpjibo-prog <phpjibo@gmail.com>
2025-12-24 12:18:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -879,6 +879,9 @@ def api_toggle_follow():
     return jsonify({"success": success, "message": message})
 
 if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
+    
     #recorder.start()          # Start background radio recording
     #start_recorder()
     print(">>> Recorder Started <<<")
